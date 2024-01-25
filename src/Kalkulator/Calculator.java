18d6bdc1e1f13d63b53dabe3bd98ca11d2d66952
Richo Dwi Afaldy <afaldydwi@gmail.com>
2024-01-25 21:35:29 +0700

    perubahan signifikan calculator

--- src/Kalkulator/Calculator.java
+++ src/Kalkulator/Calculator.java
@@ -1,44 +1,107 @@
 package Kalkulator;
-import java.util.Scanner;
 
-public class Calculator {
-    public int angkaPertama;
-    public int angkaKedua;
-    public int result;
-    public char ops;
+
+public class Calculator extends Screen{
 
     public void hitung(){
-        System.out.println("=============Input Angka Pertama=============");
-        angkaPertama = Screen.inputanPertama();
-        System.out.println("=============Input Angka Kedua=============");
-        angkaKedua = Screen.inputanKedua();
-        System.out.println("=============Input Operatornya=============");
-        System.out.println("============== +, -, *, /, % ==============");
-        ops = Screen.inputanOperator();
-        switch(ops){
-            case '+':
-                result = angkaPertama + angkaKedua;
-                System.out.println("Hasilnya adalah = " + result);
-                break;
-            case '-':
-                result = angkaPertama - angkaKedua;
-                System.out.println("Hasilnya adalah = " + result);
-                break;
-            case '*':
-                result = angkaPertama * angkaKedua;
-                System.out.println("Hasilnya adalah = " + result);
-                break;
-            case '/':
-                result = angkaPertama / angkaKedua;
-                System.out.println("Hasilnya adalah = " + result);
-                break;
-            case '%':
-                result = angkaPertama % angkaKedua;
-                System.out.println("Hasilnya adalah = " + result);
-                break;
-            default:
-                System.out.println("Harus milih operator +, -, *, /, %");
-                break;
-        }
+        Screen screen = new Screen();
+        do {
+            angkaTrue = 1;
+            while (angkaTrue == 1){
+                System.out.println("=============Input Angka Pertama=============");
+                try{
+                    angkaPertama = Screen.inputanPertama();
+                    System.out.println("Inputan Pertama Kamu = " + angkaPertama);
+                    angkaTrue = 0;
+                } catch (Exception e){
+                    System.out.println("=============WARNING!!!! ITU BUKAN ANGKA=============");
+                    angkaTrue = 1;
+                }
+            }
+            angkaTrue = 1;
+            while (angkaTrue == 1){
+                System.out.println("=============Input Angka Kedua=============");
+                try{
+                    angkaKedua = Screen.inputanKedua();
+                    System.out.println("Inputan Kedua Kamu = " + angkaKedua);
+                    angkaTrue = 0;
+                } catch (Exception e){
+                    System.out.println("=============WARNING!!!! ITU BUKAN ANGKA=============");
+                    angkaTrue = 1;
+                }
+            }
+            do {
+                System.out.println("=============Input Operatornya=============");
+                System.out.println("============== +, -, *, /, % ==============");
+                ops = Screen.inputanOperator();
+                print = 2;
+                switch (ops) {
+                    case '+':
+                        screen.setPenjumlahan(angkaPertama, angkaKedua);
+                        result = screen.getPenjumlahan();
+                        print = 0;
+                        break;
+                    case '-':
+                        screen.setPengurangan(angkaPertama, angkaKedua);
+                        result = screen.getPengurangan();
+                        print = 0;
+                        break;
+                    case '*':
+                        screen.setPerkalian(angkaPertama, angkaKedua);
+                        result = screen.getPerkalian();
+                        print = 0;
+                        break;
+                    case '/':
+                        screen.setPembagian(angkaPertama, angkaKedua);
+                        hasilPembagian = screen.getPembagian();
+                        print = 1;
+                        break;
+                    case '%':
+                        screen.setMod(angkaPertama, angkaKedua);
+                        result = screen.getMOd();
+                        print = 0;
+                        break;
+                    default:
+                        System.out.println("Harus milih operator +, -, *, /, %");
+                        break;
+                }
+                if (print == 1) {
+                    System.out.println("Hasilnya adalah = " + String.format("%.2f", hasilPembagian));
+                } else if (print == 0) {
+                    System.out.println("Hasilnya adalah = " + result);
+                } else {
+                    ;
+                }
+            } while (print == 2);
+            System.out.println("==========Apakah kamu ingin masih ingin menggunakan calculator ==========");
+            System.out.println("=======================Tentukan Pilihanmu (Yes/No)=======================");
+            do {
+                screen.setDecision();
+                decision = screen.getDecision();
+                switch (decision) {
+                    case "yes":
+                    case "Yes":
+                    case "YES":
+                    case "yEs":
+                    case "yeS":
+                    case "YEs":
+                    case "yES":
+                        resultDecision = 1;
+                        break;
+                    case "No":
+                    case "NO":
+                    case "no":
+                    case "nO":
+                        resultDecision = 0;
+                        break;
+                    default:
+                        System.out.println("==========kamu harus ketik antara (Yes/No)==========");
+                        resultDecision = 2;
+                        break;
+                }
+            }while(resultDecision == 2);
+        }while(resultDecision == 1);
+        System.out.println("============Calculator Berhasil Ditutup===========");
+        System.out.println("=====================TERIMA KASIH=================");
     }
 }
--- src/Kalkulator/Screen.java
+++ src/Kalkulator/Screen.java
@@ -2,23 +2,71 @@ package Kalkulator;
 
 import java.util.Scanner;
 
-public class Screen extends Calculator {
-    public static int inputanPertama(){
+public class Screen{
+    public long angkaPertama;
+    public long angkaKedua;
+    public long result;
+    public char ops;
+    public long hasil;
+    public long print;
+    public double hasilPembagian;
+    public long angkaTrue;
+    public String decision;
+    public long resultDecision;
+
+    public static long inputanPertama(){
         Scanner angkaPertama = new Scanner(System.in);
-        int pertama = angkaPertama.nextInt();
+        long pertama = angkaPertama.nextLong();
         return pertama;
     }
-
-    public static int inputanKedua(){
+    public static long inputanKedua(){
         Scanner angkaKedua = new Scanner(System.in);
-        int kedua = angkaKedua.nextInt();
+        long kedua = angkaKedua.nextLong();
         return kedua;
     }
-
     public static char inputanOperator(){
         Scanner operator = new Scanner(System.in);
         char ops = operator.next().charAt(0);
         return ops;
     }
+    public void setPenjumlahan(long angkaPertama, long angkaKedua){
+        hasil = angkaPertama + angkaKedua;
+        this.hasil = hasil;
+    }
+    public long getPenjumlahan(){
+        return this.hasil;
+    }
+    public void setPengurangan(long angkaPertama, long angkaKedua){
+        hasil = angkaPertama - angkaKedua;
+        this.hasil = hasil;
+    }
+    public long getPengurangan(){return this.hasil;}
+    public void setPerkalian(long angkaPertama, long angkaKedua){
+        hasil = angkaPertama * angkaKedua;
+        this.hasil = hasil;
+    }
+    public long getPerkalian(){
+        return this.hasil;
+    }
+    public void setPembagian(long angkaPertama, long angkaKedua){
+        double hasilPembagian = (double) angkaPertama / angkaKedua;
+        this.hasilPembagian = hasilPembagian;
+    }
+    public double getPembagian(){
+        return this.hasilPembagian;
+    }
+    public void setMod(long angkaPertama, long angkaKedua){
+        hasil = angkaPertama % angkaKedua;
+        this.hasil = hasil;
+    }
+    public long getMOd(){
+        return this.hasil;
+    }
+    public void setDecision(){
+        Scanner decision = new Scanner(System.in);
+        this.decision = decision.next().toString();
+    }
+    public String getDecision(){return this.decision;}
+
 
 }
